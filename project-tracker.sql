a55236fb7797c61d88e074b08e0769c4652206ad
karengcecena <karengcecena@gmail.com>
2022-10-17 18:05:44 -0700

    Lab Complete

--- project-tracker.sql
+++ project-tracker.sql
@@ -21,6 +21,42 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: karencecena
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO karencecena;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: karencecena
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO karencecena;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: karencecena
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: karencecena
 --
 
@@ -47,6 +83,26 @@ CREATE TABLE public.students (
 ALTER TABLE public.students OWNER TO karencecena;
 
 --
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: karencecena
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: karencecena
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Markov	50
+4	sdevelops	Blockly	100
+7	sdevelops	Markov	100
+\.
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: karencecena
 --
 
@@ -70,6 +126,21 @@ sdevelops	Sarah	Developer
 
 
 --
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: karencecena
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 7, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: karencecena
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: karencecena
 --
 
@@ -83,6 +154,22 @@ ALTER TABLE ONLY public.projects
 
 ALTER TABLE ONLY public.students
     ADD CONSTRAINT students_pkey PRIMARY KEY (github);
+
+
+--
+-- Name: grades grades_project_title_fkey; Type: FK CONSTRAINT; Schema: public; Owner: karencecena
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_project_title_fkey FOREIGN KEY (project_title) REFERENCES public.projects(title);
+
+
+--
+-- Name: grades grades_student_github_fkey; Type: FK CONSTRAINT; Schema: public; Owner: karencecena
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_student_github_fkey FOREIGN KEY (student_github) REFERENCES public.students(github);
 
 
 --
